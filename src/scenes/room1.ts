3ce2587f7cb155c2e58b16913cf98622694e58bf
marcelofirmino29 <marc.firmino@gmail.com>
2023-07-11 17:13:21 -0400

    melhoria room1

--- src/scenes/room1.ts
+++ src/scenes/room1.ts
@@ -1,5 +1,6 @@
 // Switch the import to use the index
-import { Door } from "../gameObjects/index";
+import utils from "../../node_modules/decentraland-ecs-utils/index";
+import { Door, MovableEntity } from "../gameObjects/index";
 import resources from "../resources";
 
 export function CreateRoom1(): void {
@@ -9,8 +10,18 @@ export function CreateRoom1(): void {
     resources.sounds.doorSqueak
   );
 
+  // A statue blocks the doorway
+  const munaStatue = new MovableEntity(
+    resources.models.muna,
+    { position: new Vector3(21.89, 10.8, 23.07) },
+    resources.sounds.moveObject1,
+    new Vector3(2, 0, 0),
+    1.5
+  );
+
   door.addComponent(
     new OnClick((): void => {
+      munaStatue.getComponent(utils.ToggleComponent).toggle();
       door.openDoor();
     })
   );
